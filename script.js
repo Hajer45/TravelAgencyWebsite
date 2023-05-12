09be3104c9800d3d57247736d33f923ad4616346
unknown <hagartalbi@gmail.com>
2023-05-12 16:13:23 +0100

    search bardone

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let navbar = document.querySelector(".navbar");
+let searchForm = document.querySelector(".search-form");
+
 document.querySelector("#menu-btn").onclick = ()=>{
     navbar.classList.add("active");
 }
@@ -7,4 +9,10 @@ document.querySelector("#nav-close").onclick = ()=>{
 }
 window.onscroll = () =>{
     navbar.classList.remove("active");
+}
+document.querySelector("#search-btn").onclick = ()=>{
+    searchForm.classList.add("active");
+}
+document.querySelector("#close-search").onclick = ()=>{
+    searchForm.classList.remove("active");
 }
